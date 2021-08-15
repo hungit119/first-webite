1521da9bb74258db256a8c396322e4e95f1abea5
hungit119 <tranduyhungdz119@gmail.com>
2021-08-15 18:35:26 +0700

    read data from multiple collection done

--- src/app/controllers/FilmController.js
+++ src/app/controllers/FilmController.js
@@ -1,17 +1,31 @@
+const { multipleMongooseToObject } = require('../../util/mongoose')
 const Film = require('../../resources/models/filmCollections');
+const FilmHD = require('../../resources/models/filmsHD.model');
+
 class FilmController {
-  index(req, res, next) {
-    Film.find({})
-      .then((Films) => {
-        const films = Films.map((film) => {
-          return film.toObject();
-        });
-        res.render('films', {films});
+  async index(req, res, next) {
+    let collections = [Film, FilmHD];
+    let enCollections = [];
+    for (let i = 0; i < collections.length; i++) {
+      await collections[i].find({}, function (err, foundCollection) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          enCollections[i] = foundCollection;
+        }
       })
-      .catch(next);
+    }
+    res.render('films', {
+      films: multipleMongooseToObject(enCollections[0]),
+      filmsHD: multipleMongooseToObject(enCollections[1])
+    })
   }
   add(req, res) {
     res.render('addFilm');
+  }
+  addDC(req,res,next){
+    res.render('addFilmDC');
   }
   data(req, res, next) {
     const film = new Film(req.body);
@@ -23,24 +37,42 @@ class FilmController {
       })
       .catch(next);
   }
-  detail(req, res, next) {
-    Film.films.findById(req.params.id)
-      .then((Films) => {
-        const films = Films.toObject();
-        res.render('detail', {films});
+  dataDC(req,res,next){
+    const filmDC = new FilmHD(req.body);
+    filmDC
+      .save()
+      .then(() => {
+        console.log('Phim Đề Cử đã được lưu vào database');
+        res.redirect('/films');
       })
       .catch(next);
   }
-  phimTC(req,res,next){
+  detail(req, res, next) {
+    Film.findById(req.params.id)
+      .then((Films) => {
+        const films = Films.toObject();
+        res.render('detail', { films });
+      })
+      .catch(next);
+  }
+  detailDC(req,res,next){
+    FilmHD.findById(req.params.id)
+      .then((Films) => {
+        const filmsDC = Films.toObject();
+        res.render('detailDC', { filmsDC });
+      })
+      .catch(next);
+  }
+  phimTC(req, res, next) {
     res.render('phimTC')
   }
-  phimHD(req,res,next){
+  phimHD(req, res, next) {
     res.render('phimHD')
   }
-  phimNMCC(req,res,next){
+  phimNMCC(req, res, next) {
     res.render('phimNMCC')
   }
-  phimCR(req,res,next){
+  phimCR(req, res, next) {
     res.render('phimCR')
   }
 }
--- src/routes/films.js
+++ src/routes/films.js
@@ -9,9 +9,12 @@ route.use('/phimHD',filmController.phimHD)
 route.use('/phimNMCC',filmController.phimNMCC)
 route.use('/phimCR',filmController.phimCR)
 route.use('/creat',filmHDController.creat)
+route.use('/detailDC/:id',filmController.detailDC)
 route.use('/detail/:id',filmController.detail)
 route.use('/data',filmController.data)
+route.use('/dataDC',filmController.data)
 route.use('/add',filmController.add)
+route.use('/addDC',filmController.addDC)
 route.use('/',filmController.index)
 
 module.exports = route
