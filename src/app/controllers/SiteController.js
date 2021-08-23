6f9255fc5d51462ad101d77230b7641724f9ed7b
hungit119 <tranduyhungdz119@gmail.com>
2021-08-23 11:00:36 +0700

    Cac phan trong 1 trang : DONE !!

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -16,26 +16,59 @@ class SiteController {
           })
           .then(docs2s => {
             films.find({
-                theLoai: '6121f64d83d7900b801af8e3'
+                theLoai: '6121f64d83d7900b801af8e3' //phim tình cảm
               })
               .populate('namSuatBan')
               .populate('theLoai')
               .then(docs3s => {
-                const data1s = docs1s.map(docs1 => {
-                  return docs1.toObject();
-                });
-                const data2s = docs2s.map(function (docs2) {
-                  return docs2.toObject();
-                });
-                const data3s = docs3s.map(function (docs3) {
-                  return docs3.toObject();
-                });
-                res.render('home', {
-                  data1: data1s,
-                  data2: data2s,
-                  data3: data3s
-                });
-                console.log("Render Success !!!");
+                films.find({
+                    theLoai: '6121f6602e18d81fac40ebea' //phim hành động
+                  })
+                  .populate('namSuatBan')
+                  .populate('theLoai')
+                  .then(docs4s => {
+                    films.find({
+                        theLoai: '6121f6765a14152fe45773a4' //phim nước mắt 
+                      })
+                      .populate('namSuatBan')
+                      .populate('theLoai')
+                      .then(docs5s => {
+                        films.find({
+                            theLoai: '61231349f5b1672f983aede8' //phim hoạt hình
+                          })
+                          .populate('namSuatBan')
+                          .populate('theLoai')
+                          .then(docs6s => {
+                            const data1s = docs1s.map(docs1 => {
+                              return docs1.toObject();
+                            });
+                            const data2s = docs2s.map(function (docs2) {
+                              return docs2.toObject();
+                            });
+                            const data3s = docs3s.map(function (docs3) {
+                              return docs3.toObject();
+                            });
+                            const data4s = docs4s.map(function (docs4) {
+                              return docs4.toObject();
+                            });
+                            const data5s = docs5s.map(function (docs5) {
+                              return docs5.toObject();
+                            });
+                            const data6s = docs6s.map(function (docs6) {
+                              return docs6.toObject();
+                            });
+                            res.render('home', {
+                              data1: data1s,
+                              data2: data2s,
+                              data3: data3s,
+                              data4: data4s,
+                              data5: data5s,
+                              data6: data6s,
+                            });
+                            console.log("Render Success !!!");
+                          })
+                      })
+                  })
               })
           })
           .catch(next);
--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-
 router(app)
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
