b7ef9d43ede5768b7147a0ae311feba910513072
hungit119 <tranduyhungdz119@gmail.com>
2021-08-29 13:57:10 +0700

    danh muc 2 done

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -5,51 +5,57 @@ class SiteController {
     // Tìm phim trong collection:
     //phim mới
     const phimmoi = await films.find({
-        category: '61221735b39d0a3098a69f47'
-      }, function () {
-      })
+      category: '61221735b39d0a3098a69f47'
+    }, function () {
+    })
       .populate('theLoai')
       .populate('namSuatBan')
     //phim đề cử
     const phimdecu = await films.find({
-        category: '61220f53e917943b903767f9'
-      }, function () {
-      })
+      category: '61220f53e917943b903767f9'
+    }, function () {
+    })
       .populate('theLoai')
       .populate('namSuatBan')
     //phim tình cảm
     const phimtinhcam = await films.find({
-        theLoai: '6121f64d83d7900b801af8e3'
-      }, function () {
-      })
+      theLoai: '6121f64d83d7900b801af8e3'
+    }, function () {
+    })
       .populate('theLoai')
       .populate('namSuatBan')
     //phim hành động
     const phimhanhdong = await films.find({
-        theLoai: '6121f6602e18d81fac40ebea'
-      }, function () {
-      })
+      theLoai: '6121f6602e18d81fac40ebea'
+    }, function () {
+    })
       .populate('theLoai')
       .populate('namSuatBan')
     // phim nước mắt 
     const phimnuocmat = await films.find({
-        theLoai: '6121f6765a14152fe45773a4'
-      }, function () {
-      })
+      theLoai: '6121f6765a14152fe45773a4'
+    }, function () {
+    })
       .populate('theLoai')
       .populate('namSuatBan')
     // phim hoạt hình 
     const phimhoathinh = await films.find({
-        theLoai: '61231349f5b1672f983aede8'
-      }, function () {
-      })
+      theLoai: '61231349f5b1672f983aede8'
+    }, function () {
+    })
       .populate('theLoai')
       .populate('namSuatBan')
     //phim chiếu rạp 
     const phimchieurap = await films.find({
-        theLoai: '6121f68794fe3633bcb45559'
-      }, function () {
-      })
+      theLoai: '6121f68794fe3633bcb45559'
+    }, function () {
+    })
+      .populate('theLoai')
+      .populate('namSuatBan')
+    const phimthanhxuan = await films.find({
+      theLoai: '612b2a0bf570944a284bd88a'
+    }, function () {
+    })
       .populate('theLoai')
       .populate('namSuatBan')
     // Map phim thành obj:
@@ -74,6 +80,9 @@ class SiteController {
     const phimchieuraps = phimchieurap.map(data => {
       return data.toObject();
     })
+    const phimthanhxuans = phimthanhxuan.map(data => {
+      return data.toObject();
+    })
     //render ra view home
     res.render('home', {
       data1: phimmois,
@@ -83,6 +92,7 @@ class SiteController {
       data5: phimnuocmats,
       data6: phimhoathinhs,
       data7: phimchieuraps,
+      data8: phimthanhxuans,
     })
   }
 }
